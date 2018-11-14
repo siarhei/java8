23f06f4b1dc04aa88dd7dbbc78b12eeb3ef04465
siarhei <s.siarhei@gmail.com>
2018-11-14 21:10:58 +0300

    ch03 ex07 completed

--- java8-impatient-book/chapter03/src/main/java/ch03/Ex07.java
+++ java8-impatient-book/chapter03/src/main/java/ch03/Ex07.java
@@ -1,7 +1,9 @@
 package ch03;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Optional;
+import java.util.function.Function;
 
 /**
  * Write a method that generates a Comparator<String> that can be normal or reversed,
@@ -12,27 +14,34 @@ import java.util.Optional;
  */
 public class Ex07 {
 
-    private enum Order {
-        NORMAL(true),
-        REVERSE(false),
-        CASE_SENSITIVE(true),
-        CASE_INSENSITIVE(false)
+    private static Comparator<String> generate(boolean reverse, Boolean caseSensitive, Boolean spaceSensitive) {
+        //direct mapping
+        Function<String, String> keyExtractor = Function.identity();
+        if (Boolean.FALSE.equals(caseSensitive)) {
+            //case insensitive
+            keyExtractor = keyExtractor.andThen(String::toLowerCase);
+        }
+        if (Boolean.FALSE.equals(spaceSensitive)) {
+            //space insensitive
+            keyExtractor = keyExtractor.andThen(String::trim);
+        }
 
-        private final boolean natural;
-        Order(boolean natural) {
-            this.natural = natural;
+
+        Comparator<String> comparator = Comparator.comparing(keyExtractor);
+        return reverse ? comparator.reversed() : comparator;
+
+    }
+
+    private static void print(String[] words) {
+        for (String word : words) {
+            System.out.printf("->%s<-\n", word);
         }
     }
 
-    private Comparator<String> generate(Boolean natural, Boolean caseSensitive, Boolean spaceSensitive) {
-        Optional<Comparator<String>> naturalCmp = Optional.ofNullable(natural)
-                .map(flag -> flag ? Comparator.naturalOrder() : Comparator.reverseOrder());
-        Optional<Comparator<String>> caseSensCmp =Optional.ofNullable(natural)
-                .map(flag -> flag ? Comparator.<String>naturalOrder() : (a, b) -> Comparator.comparing((String key) -> key.toLowerCase()) );
-    }
-
     public static void main(String[] args) {
-        byte b = 0b01101010;
-        System.out.println(Integer.toBinaryString(b & ));
+        String[] words = {"AbC1", "ABC", " abc3", " Abc4", "ABC "};
+
+        Arrays.sort(words, generate(true, false, false));
+        print(words);
     }
 }

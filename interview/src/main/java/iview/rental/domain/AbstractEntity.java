63f1d1ee5fab98dd6a926cb1162cf2391e5a1046
Siarhei Shchahratsou <s.siarhei@gmail.com>
2020-06-14 13:25:55 +0300

    id generator

--- interview/src/main/java/iview/rental/domain/AbstractEntity.java
+++ interview/src/main/java/iview/rental/domain/AbstractEntity.java
@@ -1,22 +1,22 @@
 package iview.rental.domain;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import iview.rental.IdGenerator;
+
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public abstract class AbstractEntity implements Entity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private final Long id;
+
+    public AbstractEntity() {
+        this.id = IdGenerator.getNextId();
+    }
 
     @Override
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 }
--- interview/src/main/java/iview/rental/domain/Entity.java
+++ interview/src/main/java/iview/rental/domain/Entity.java
@@ -2,4 +2,8 @@ package iview.rental.domain;
 
 public interface Entity {
     Long getId();
+
+    default void print() {
+
+    }
 }
